6f3b988a86d5d56ad3c0893e0b2d060792da23c9
Jeroen Breen <jeroen@innouveau.nl>
2021-04-06 09:38:31 +0200

    initial files

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+    import Flex from "./components/Flex";
+    export default {
+        name: 'App',
+        components: {Flex}
+    }
+</script>
+
+
+<template>
+    <div class="app">
+        <h1>
+            Test app to see what transpiling does
+        </h1>
+        <Flex/>
+    </div>
+</template>
+
+
+<style lang="scss">
+    .app {
+
+        h1 {
+
+        }
+
+        .example {
+
+            .example__head {
+                font-weight: 700;
+                border-left: 2px solid red;
+                padding-left: 8px;
+                margin-bottom: 12px;
+            }
+
+            .example__body {
+                border: 1px solid #ddd;
+                padding: 8px;
+            }
+        }
+    }
+</style>
---
+++ src/components/Flex.vue
@@ -0,0 +1,44 @@
+<script>
+    export default {
+        name: 'Flex',
+        components: {},
+        computed: {},
+        methods: {}
+    }
+</script>
+
+
+<template>
+    <div class="Flex example">
+        <div class="example__head">
+            1) CSS: Flex
+        </div>
+        <div class="example__body">
+            <div class="flex-container">
+                <div class="flex-element">1</div>
+                <div class="flex-element">2</div>
+                <div class="flex-element">3</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss">
+    .Flex {
+
+        .flex-container {
+            display: flex;
+
+            .flex-element {
+                flex-grow: 1;
+                background: #ddd;
+                border: 1px solid #fff;
+                display: flex;
+                justify-content: center;
+                height: 32px;
+                align-items: center;
+            }
+        }
+    }
+</style>
